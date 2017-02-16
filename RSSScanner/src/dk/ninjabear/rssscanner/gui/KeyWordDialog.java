e22dc6e58aa7c82e32e08b283a4d7b13b9097349
Asbjørn Skjødsholm Madsen <asbjoern1982@gmail.com>
2017-02-16 21:33:11 +0100

    fixed dialog layout

--- RSSScanner/src/dk/ninjabear/rssscanner/gui/KeyWordDialog.java
+++ RSSScanner/src/dk/ninjabear/rssscanner/gui/KeyWordDialog.java
@@ -1,13 +1,17 @@
 package dk.ninjabear.rssscanner.gui;
 
 import dk.ninjabear.rssscanner.service.Service;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -17,32 +21,38 @@ public class KeyWordDialog extends Stage {
 	public KeyWordDialog() {
 		initModality(Modality.APPLICATION_MODAL);
 		setTitle("Select Sudoku");
-		BorderPane borderPane = new BorderPane();
-		initContent(borderPane);
-		setScene(new Scene(borderPane));
+		GridPane pane = new GridPane();
+		initContent(pane);
+		setScene(new Scene(pane));
 	}
 	
 	private final TextField addTextField = new TextField();
 	private final ListView<String> wordListView = new ListView<>();
 	
-	private void initContent(BorderPane root) {
+	private void initContent(GridPane root) {
 		root.setPadding(new Insets(10));
+		root.setVgap(5);
+		root.setHgap(5);
+		
 
 		Button addButton = new Button("+");
 		Button removeButton = new Button("-");
 		addButton.setOnAction(e -> controller.addAction());
 		removeButton.setOnAction(e -> controller.removeAction());
+
+		HBox.setHgrow(addTextField, Priority.ALWAYS);
 		HBox top = new HBox();
 		top.setSpacing(5);
 		top.getChildren().addAll(addTextField, addButton, removeButton);
-		root.setTop(top);
+		root.add(top, 0, 0);
 		
-		root.setCenter(wordListView);
+		root.add(wordListView, 0, 1);
 		
 		Button closeButton = new Button("close");
 		closeButton.setDefaultButton(true);
 		closeButton.setOnAction(e -> controller.closeAction());
-		root.setBottom(closeButton);
+		GridPane.setHalignment(closeButton, HPos.RIGHT);
+		root.add(closeButton, 0, 2);
 		
 		controller.updateControls();
 	}
